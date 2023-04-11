7fb6a425e6b8a69ab8b688063fc4a7bc41c25af4
somesh-py <someshsharma615@gmail.com>
2023-04-11 10:44:57 +0530

    my first fast api

---
+++ user/app/models.py
@@ -0,0 +1,12 @@
+from tortoise.models import Model
+from tortoise import Tortoise,fields
+
+class User(Model):
+    id=fields.IntField(pk=True)
+    email=fields.CharField(50,unique=True)
+    name=fields.CharField(80)
+    phone=fields.CharField(10)
+    password=fields.CharField(250)
+
+
+Tortoise.init_models(['app.models'],'models')
